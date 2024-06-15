cc270573624fa59d5d800b7441f5c4fc969ea07d
Naol Ketema <naolketema55@gmail.com>
2024-06-15 22:27:58 +0300

    asklf

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,4 @@
--- A SQL script that ranks country origins of bands, ordered by 
--- the number of (non-unique) fans
--- Column names must be: origin and nb_fans
--- Your script can be executed on any database
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
 SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,13 @@
--- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
--- Quantity in the table items can be negative.
--- Context: Updating multiple tables for one action from your application can generate issue: network disconnection,
--- crash, etc… to keep your data in a good shape, let MySQL do it for you!
-
-CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
